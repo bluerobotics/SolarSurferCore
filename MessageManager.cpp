23fab07b5bb47308171bf584d453cbdfd9535818
Rustom Jehangir <rustyj110@gmail.com>
2014-07-22 07:13:18 -0700

    Modified TestSatcom CMakeLists file to automatically compile the test version of MessageManager.

--- MessageManager.cpp
+++ MessageManager.cpp
@@ -7,8 +7,6 @@
 #include "RemoteControl.h"
 #include "Thruster.h"
 
-//#define TEST_ONLY
-
 namespace Msg {
 	MessageType::tlmstatus tlmstatus;
 	MessageType::tlmdiagnostic tlmdiagnostic;
@@ -25,7 +23,34 @@ namespace MessageManager {
 	}
 	
 	void updateFields() {
-#ifndef TEST_ONLY
+#ifdef MM_TEST_ONLY
+
+		Msg::tlmstatus.version					=					1;
+		Msg::tlmstatus.format						=					0;
+		Msg::tlmstatus.latitude					=					33.4;
+		Msg::tlmstatus.longitude 				=					-118.8;
+		Msg::tlmstatus.fix							=					1;
+		Msg::tlmstatus.time							=					12345678;
+		Msg::tlmstatus.altitude					=					0;
+		Msg::tlmstatus.course						=					900;
+		Msg::tlmstatus.heading					=					1800;
+		Msg::tlmstatus.p_solar					=					120;
+		Msg::tlmstatus.p_batt						=					15;
+		Msg::tlmstatus.p_left						=					49;
+		Msg::tlmstatus.p_right					=					51;
+		Msg::tlmstatus.rotations				=					123;
+		Msg::tlmstatus.rpm_left					=					100;
+		Msg::tlmstatus.rpm_right				=					101;
+		Msg::tlmstatus.swellHeight			=					53;
+		Msg::tlmstatus.commandCount			=					1;
+		Msg::tlmstatus.tempAir					=					25;
+		Msg::tlmstatus.tempWater				=					22;
+		Msg::tlmstatus.pressure					=					10130;
+		Msg::tlmstatus.pH								=					820;
+		Msg::tlmstatus.telemetryCount		=					4;
+
+#else		
+
 		Msg::tlmstatus.version					=					1;
 		Msg::tlmstatus.format						=					3;
 		Msg::tlmstatus.latitude					=					GPS_UBX::latitude;
@@ -73,32 +98,6 @@ namespace MessageManager {
 		Msg::tlmdiagnostic.isOther						=					RemoteControl::isOther();	
 		Msg::tlmdiagnostic.rcSteering					=					RemoteControl::getSteering();
 		Msg::tlmdiagnostic.rcPower						=					RemoteControl::getPower();
-
-#else
-		Msg::tlmstatus.version					=					1;
-		Msg::tlmstatus.format						=					0;
-		Msg::tlmstatus.latitude					=					33.4;
-		Msg::tlmstatus.longitude 				=					-118.8;
-		Msg::tlmstatus.fix							=					1;
-		Msg::tlmstatus.time							=					12345678;
-		Msg::tlmstatus.altitude					=					0;
-		Msg::tlmstatus.course						=					900;
-		Msg::tlmstatus.heading					=					1800;
-		Msg::tlmstatus.p_solar					=					120;
-		Msg::tlmstatus.p_batt						=					15;
-		Msg::tlmstatus.p_left						=					49;
-		Msg::tlmstatus.p_right					=					51;
-		Msg::tlmstatus.rotations				=					123;
-		Msg::tlmstatus.rpm_left					=					100;
-		Msg::tlmstatus.rpm_right				=					101;
-		Msg::tlmstatus.swellHeight			=					53;
-		Msg::tlmstatus.commandCount			=					1;
-		Msg::tlmstatus.tempAir					=					25;
-		Msg::tlmstatus.tempWater				=					22;
-		Msg::tlmstatus.pressure					=					10130;
-		Msg::tlmstatus.pH								=					820;
-		Msg::tlmstatus.telemetryRate		=					2;
-		Msg::tlmstatus.telemetryCount		=					4;
 #endif
 	}
 	
